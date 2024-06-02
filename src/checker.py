fc6598af344d890256c7f362c0a7bbcf2e085479
Abhijeet <84558686+AJTimePyro@users.noreply.github.com>
2024-06-03 01:52:39 +0530

    Set file opening encoding to utf-8

--- src/checker.py
+++ src/checker.py
@@ -57,7 +57,7 @@ class CrunchyrollChecker:
         self._checker()
 
     def _checker(self):
-        file = open(self.filename, errors='ignore')
+        file = open(self.filename, encoding="utf8" ,errors='ignore')
         for line in file.readlines():
             loginDetail = self._filterEmailPass(line)
             if loginDetail:
